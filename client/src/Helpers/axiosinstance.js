228a13f58f74ac24a4b573fb167b6bec32adc9a5
anirbanbhunia <anirbanbhunia7@gmail.com>
2024-08-31 16:22:51 +0530

    redux-toolkit basic setup

---
+++ client/src/Helpers/axiosinstance.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const baseUrl = process.env.SERVER_URL
+
+const axiosInstance = axios.create()
+
+axiosInstance.defaults.baseURL = baseUrl //This sets the base URL for all requests made with this Axios instance. Instead of writing the full URL for each request, you can now just specify the endpoint, and Axios will automatically prepend the base URL.
+
+axiosInstance.defaults.withCredentials = true //This is a configuration option that tells Axios to include credentials (like cookies, authorization headers, etc.) in cross-origin requests. It’s often used when making requests to APIs that require authentication.
+
+export default axiosInstance
